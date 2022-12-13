eab14a1025ed8756c4b4c29fb28ad2bc38b61943
Roshan Kumar <roshanfrmec@gmail.com>
2022-12-13 20:36:17 +0100

    Initial packaging and repo structure added

---
+++ src/main/java/com/service/management/account/danske/AccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.service.management.account.danske;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountManagementApplication.class, args);
+    }
+
+}
